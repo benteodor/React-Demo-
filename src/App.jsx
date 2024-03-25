d6cd314005e1a2298875b19fd79b3df7b6bf30f1
Popa Teodor <teodor.popa96@gmail.com>
2024-03-25 14:28:33 +0200

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ function App() {
     { id:1, name: "Teodor", title:"student" ,ocation:"Helsinki"},           { id:2, name: "Marius",title:"student" ,ocation:"Helsinki"},            { id:3, name: "Dana", title:"student", ocation:"Helsinki" },
   ]);
  
+  const clickHandler = () => {
+    console.log("Hi There");
+  };
+
+
   return (
     <>
     <p>Counter: {counter}</p>
-
+    <button onClick={clickHandler}>Don't click me</button>
     {persons.map((person) => (
     <Box key={person.id}        name={person.name}
     title={person.title}
